ff2fa37caf5e60dcbbc2da5c0eb2ad27dfab4cd6
denisshovkoplas <denisshovkoplas@MacBook-Pro-Denis.local>
2024-12-02 15:11:06 +0100

    Addition of Subtasks

---
+++ subtask1.py
@@ -0,0 +1,10 @@
+def largest_square (n):
+    q = 1
+    a = 1
+    while q<=n:
+        a = a+1
+        q = a*a
+    a = a-1
+    q = a*a
+    print(q)
+
---
+++ subtask2.py
@@ -0,0 +1,25 @@
+n = 0
+s = 0
+a = 0
+m = 1234567890
+
+print("Enter numbers for the sequence:")
+
+while True:
+    x = int(input("Enter number:"))
+    if x == -1:
+        break
+    n += 1
+    s += x
+    if m > x:
+        m = x
+
+if n == 0:
+    m = -1
+    a = -1
+else:
+    a = s / n
+print(f"Count(n): {n}") 
+print(f"Sum(s): {s}") 
+print(f"Minimum(m): {m}") 
+print(f"Mean(a): {a}")
